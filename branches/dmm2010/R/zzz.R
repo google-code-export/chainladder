c58fc62f15b0fd093627fe4e3fbdb82df55de819
chiefmurphy <chiefmurphy@e5f87290-e53f-0410-917e-5545ed4be688>
2010-11-19 23:27:30 +0000

    Creating a working branch of ChainLadder

---
+++ branches/dmm2010/R/zzz.R
@@ -0,0 +1,21 @@
+.onLoad<- function(lib, pkg, ...)
+{
+    require(utils)
+    cat("\n",paste("ChainLadder version", packageDescription("ChainLadder")$Version,
+                   "by:\nMarkus Gesmann <markus.gesmann@gmail.com>",
+                   "\nWayne (Yanwei) Zhang <actuaryzhang@uchicago.edu>",
+                   "\nDaniel Murphy <danielmarkmurphy@gmail.com>",
+                   "\n\n"),
+        "Type library(help='ChainLadder') or ?ChainLadder\n",
+        "to see overall documentation.\n\n",
+        "Type demo(ChainLadder) to get an idea of the functionality of this package.\n\n",
+        "See demo(package='ChainLadder') for a list of more demos.\n\n",
+        "Feel free to send us an email if you would like to keep ",
+        "informed of\nnew versions or if you have any feedback, ",
+        "ideas, suggestions or ",
+        "would\nlike to collaborate.\n\n",
+        "More information is available on the ChainLadder project web-site:\n",
+        "http://code.google.com/p/chainladder/\n\n",
+        sep='')
+  invisible()
+}
